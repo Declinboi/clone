9ea0ebe68a7c4005cda30c2ca5f6e213b72b2078
declinboi <jacobclinton2234@gmail.com>
2025-07-21 02:48:15 +0100

    runner

---
+++ clone/android/app/src/main/kotlin/com/cloney1/clone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cloney1.clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ clone/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:clone/app/app.dart';
+import 'package:clone/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
