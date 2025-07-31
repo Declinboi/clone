9c9092bbbb197e5d93ade6009126dff184fbcb0c
declinboi <jacobclinton2234@gmail.com>
2025-07-31 23:09:42 +0100

    app ui

--- clone/packages/app_ui/lib/app_ui.dart
+++ clone/packages/app_ui/lib/app_ui.dart
@@ -1,4 +1,8 @@
 /// A Very Good Project created by Very Good CLI.
+// ignore_for_file: eol_at_end_of_file
+
 library;
 
-export 'src/app_ui.dart';
+export 'src/colors/app_colors.dart';
+export 'src/generated/generated.dart';
+export 'src/theme/theme.dart';
